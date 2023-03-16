c617b4eae91e67f4772562f008692d3b6eace345
KMchaudhari7995 <kmchaudhari7912@gmail.com>
2023-03-16 23:49:15 +0000

    My new Homework5

---
+++ src/Program1.java
@@ -0,0 +1,24 @@
+/**
+ * 1. Write a program that prints your BIO Data.
+ */
+public class Program1 {
+
+
+        public static void main(String[] argh) {
+            System.out.println("              BIO DATA                ");
+            System.out.println("Name              : Khushbu Chaudhari");
+            System.out.println("Mobile            : 09876543210");
+            System.out.println("Email             : Khushbu123@gmail.com");
+            System.out.println("Gender            : Female");
+            System.out.println("Date of Birth     : 01.06.1995");
+            System.out.println("Marital Status    : Married");
+            System.out.println("Languages Known   : English, Hindi and Gujarati");
+            System.out.println("Qualification     : Software Tester");
+            System.out.println("Experience        : Agile Tester with 2 years of experience");
+            System.out.println("Address           : WD17 2LA, Watford ");
+            System.out.println("Place             : London");
+
+        }
+    }
+
+
